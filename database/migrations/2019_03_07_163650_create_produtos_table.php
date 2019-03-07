db6890f21e3a95e72e4346466e1dc79cc8b2501b
ALUNO <fabriciolvceet@gmail.com>
2019-03-07 14:30:22 -0300

    first

---
+++ database/migrations/2019_03_07_163650_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->String('descricao');
+            $table->String('cor');
+            $table->Decimal('preco',5,2);
+            $table->String('peso');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
